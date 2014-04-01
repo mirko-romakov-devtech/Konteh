3a0e510e930965b8f91a0968f2a89b5d0bb03596
Dejan Obradovic <dejan.obradovic@devtechgroup.com>
2014-04-01 11:56:07 +0200

    Changed home directory of the challengers. Added new crypt libs to VM. Few improvements to link creation.

--- KontehVM/api.php
+++ KontehVM/api.php
@@ -3,51 +3,52 @@ $ssc = $_POST['supersecretkey'];
 $guid = $_POST['guid'];
 
 if(!isset($ssc, $guid)){
-	echo "Dalje neces moci!";
-	die();
+        echo "Dalje neces moci!";
+        die();
 }
 
 $ssc = $_POST['supersecretkey'];
 $key = "supersecretkey";
 
 for ($i = 0; $i < 14; $i++) {
-	$key = md5($key);
+        $key = md5($key);
 }
 
 if($ssc != $key){
-	echo "SUPER SECRET KEY IS INVALID!";
-	die();
+        echo "SUPER SECRET KEY IS INVALID!";
+        die();
 }
 
 require_once 'config.php';
 require_once 'EncryptionHelper.php';
 require_once 'DatabaseHandler.php';
 
-$path = '/home/testuser/home/usertest/';
+
+$path = '/home/contexuser/home/testcontex/';
 if(file_exists($path.$guid."/victory.txt")){
-	echo "LIAR LIAR PANTS ON FIRE!";
-	die();
+        echo "LIAR LIAR PANTS ON FIRE!";
+        die();
 }
 
 mkdir($path.$guid);
 
 try {
-	$encryptor = new EncryptionHelper(DB_HOST, DB_NAME, DB_USER, DB_PASS);
-	$linkModel = new LinkModel();
-	$linkModel->Action = LinkAction::ACTIVATION;
-	$linkModel->GUID = $guid;
-	$linkModel->Used = 0;
-	$kobaja = $encryptor->encryptObject($linkModel);
+        $encryptor = new EncryptionHelper(DB_HOST, DB_NAME, DB_USER, DB_PASS);
+        $linkModel = new LinkModel();
+        $linkModel->Action = LinkAction::ACTIVATION;
+        $linkModel->GUID = $guid;
+        $linkModel->Used = 0;
+        $kobaja = $encryptor->encryptObject($linkModel);
 
-	$dbHandler = DatabaseHandler::WithDefaultConfig();
-	$dbHandler->insertActivation($linkModel, $kobaja);
-	
-	$name = $path.$guid."/victory.txt";
-	$handle = fopen($name, "w");
-	fwrite($handle, "http://challenge.devtechgroup.com/youwin.php?key=".$kobaja);
-	fclose($handle);
-	
+        $dbHandler = DatabaseHandler::WithDefaultConfig();
+        $dbHandler->insertActivation($linkModel, $kobaja);
+
+        $name = $path.$guid."/victory.txt";
+        $handle = fopen($name, "w");
+        fwrite($handle, "http://challenge.devtechgroup.com/youwin.php?key=".$kobaja.PHP_EOL);
+        fclose($handle);
+
 } catch (Exception $ex) {
-	echo "Error occured: ".$ex->getMessage();
+        echo "Error occured: ".$ex->getMessage();
 }
 echo "Bravo!";
