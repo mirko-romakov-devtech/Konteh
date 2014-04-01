3f9b4f71a05496b5664910390ce1a9d7901e3a90
Dejan Obradovic <dejan.obradovic@devtechgroup.com>
2014-04-01 19:57:19 +0200

    Added key check to terminal access. Added new getCandidateVNCDetails function to retrieve VNC details. Tuned applet.conf.

--- KontehVM/terminal.php
+++ KontehVM/terminal.php
@@ -1,10 +1,50 @@
-<html>
-  <body>
-    <applet CODEBASE="."
-            ARCHIVE="jta26.jar"
-            CODE="de.mud.jta.Applet"
-            WIDTH=800 HEIGHT=600>
-            <param name="config" value="applet.conf">
-    </applet>
-  </body>
-</html>
+<?php
+//error_reporting( E_ALL );
+//ini_set('display_errors', 1);
+
+	if(isset($_GET['key'])){
+		checkKey($_GET['key']);
+	} else {
+		exit('VMKey Not Properly set');
+	}
+
+	function checkKey($asAuthKey){
+		
+		require_once('DatabaseHandler.php');
+		require_once('EncryptionHelper.php');
+		require_once('config.php');
+
+		$loEncHelper = new EncryptionHelper(DB_HOST, DB_NAME, DB_USER, DB_PASS);
+		$loDBHelper = DatabaseHandler::WithDefaultConfig();
+
+
+		$a = $loEncHelper->decryptObject($asAuthKey);
+		$a->GUID = 14;
+
+		$b = $loDBHelper->getCandidateVNCDetails($a->GUID);
+		if($_POST['username'] == $b['vnc_username'] and $_POST['password'] == $b['vnc_password']){
+			echo '<html>
+					  <body>
+					    <applet CODEBASE="."
+					            ARCHIVE="jta26.jar"
+					            CODE="de.mud.jta.Applet"
+					            WIDTH=800 HEIGHT=600>
+					            <param name="config" value="applet.conf">
+					    </applet>
+					  </body>
+					</html>';
+		}else {
+			//var_dump($asAuthKey);
+			echo '<html>
+				<body>
+					<form action="'.$_SERVER['PHP_SELF'].'?key='.$asAuthKey.'" method="POST">
+						<input type="text" name="username"/>
+						<input type="password" name="password"/>
+						<input type="submit" value="Pokidaj me jako" />
+					</form>
+				</body>
+				</html>';
+		}
+	}
+?>
+
