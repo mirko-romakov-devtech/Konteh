30fa09a8d242296998d6d0f59300faddc70908b5
atila-bala-devtech <atila.bala@devtechgroup.com>
2014-04-01 18:03:40 +0200

    updated email for challenge-adminpanel. Link in email is now encrypted, and passed in email :)

--- KontehAdmin/controllers/email_controller.php
+++ KontehAdmin/controllers/email_controller.php
@@ -35,25 +35,29 @@ class EmailController {
 		$linkModel->Used = 0;
 
 		$encryptionObject = new EncryptionHelper(DB_HOST, DB_NAME, DB_USER, DB_PASS);
-		var_dump("encryptionObject instantiated");
+		
 		$encryptionLink = $encryptionObject->encryptObject($linkModel);
-		var_dump("kobaja: ".$encryptionLink);
+		$fullyEncyptedLink = base64_encode("http://challenge.devtechgroup.com/index.php?key=" . $encryptionLink);
+		
 		$this->mail->addAddress($email_address, $first_name." ".$last_name);  // Add a recipient
 
 		$this->mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 		//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 		//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 		$this->mail->isHTML(true);                                  // Set email format to HTML
-
+		
 		$this->mail->Subject = EMAIL_SUBJECT;
-		$this->mail->Body    = 	"Dear ".$first_name." ".$last_name.", <br/><br/> Welcome to the Konteh <br/><br/>
-				To start please click at the following link: <br/>
-				<a target='_blank' href='http://challenge.devtechgroup.com/index.php?key=".$encryptionLink."'>http://challenge.devtechgroup.com/index.php?key=".$encryptionLink."</a><br/>
-						<br/><br/><br/>
-						Thank you,
-						<br/>Devtech
-						<br/>
-						";
+		$this->mail->Body    = 	"Dear ".$first_name." ".$last_name.", <br/><br/> 
+								Welcome to the Konteh <br/><br/>
+								In order to start the challenge, you will need to decode this ugly string. Nobody likes ugly strings. <br/>
+								<b>".$fullyEncyptedLink."</b>
+								<br/><br/>
+								<p>Hint: It's <b>BASE</b>-ic encoding. Some kind of <b>64</b> encoding...dunno… :)</p>
+								<br/>
+								Thank you,
+								<br/>Devtech
+								<br/>
+								";
 		$this->mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 		var_dump("before sending");
